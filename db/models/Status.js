72c8e4da10465a79273abb4dfb63c850eafa7ff2
igajduk <goloso4ok@gmail.com>
2019-05-28 18:52:02 +0300

    reports

---
+++ db/models/Status.js
@@ -0,0 +1,25 @@
+const tableName = 'status';
+
+const foreignKeys = {
+    client: 'statusId',
+};
+
+module.exports = (sequelize, DataTypes) => {
+    const Status = sequelize.define(tableName, {
+            name: DataTypes.STRING,
+            color: DataTypes.STRING
+        }, {});
+    Status.associate = function (models) {
+        Status.hasMany(models.client, {foreignKey: foreignKeys.client});
+    };
+
+    Status.tableName = tableName;
+
+    Status.notUpdatableFields = [];
+    Status.requiredFileds = [
+        'name',
+        'color'
+    ];
+
+    return Status;
+};
