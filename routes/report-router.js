0b57d27ecd12fdf703b8588297f46cd8efdfec9c
igajduk <goloso4ok@gmail.com>
2019-05-28 19:04:32 +0300

    reports

---
+++ routes/report-router.js
@@ -0,0 +1,18 @@
+let router = require('express').Router();
+let controllers = require('../сontrollers');
+let guard = require('node-auth-guard');
+let ROLES = require('../config/roles');
+
+router.route('/')
+    .get(guard.roles(ROLES.BOSS_ROLE, ROLES.MANAGER_ROLE), controllers.report.getAll);
+//     .post(guard.roles(ROLES.BOSS_ROLE, ROLES.MANAGER_ROLE), controllers.payment.create);
+//
+// router.route('/:id')
+//     .get(guard.roles(ROLES.BOSS_ROLE, ROLES.MANAGER_ROLE), controllers.payment.getById)
+//     .put(guard.roles(ROLES.BOSS_ROLE, ROLES.MANAGER_ROLE), controllers.payment.update)
+//     .delete(guard.roles(ROLES.BOSS_ROLE, ROLES.MANAGER_ROLE), controllers.payment.remove);
+//
+// router.post('/:id/upload', controllers.payment.upload);
+// router.post('/:id/create', controllers.payment.createFile);
+
+module.exports = router;
